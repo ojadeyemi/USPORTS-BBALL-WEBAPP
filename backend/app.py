6c1d1eff61aeaf1abe4edb4a2dbe840d956291a9
OJ Adeyemi <bukunmiojie@gmail.com>
2024-03-20 00:59:14 -0400

    updated style with vanilla css

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ class Team(db.Model):
     games_played = db.Column(db.Integer)
     total_wins = db.Column(db.Integer)
     total_losses = db.Column(db.Integer)
-    win_percentage = db.Column(db.Float(3))
+    win_percentage = db.Column(db.Float(precision=1))
     streak = db.Column(db.String(50))
 
 
