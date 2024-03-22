1c9e6b499ee8163f585ab970a2b02abd70058aa0
OJ Adeyemi <bukunmiojie@gmail.com>
2024-03-21 22:25:31 -0400

    update league script

--- backend/static/js/script.js
+++ backend/static/js/script.js
@@ -1,14 +1,9 @@
-
-/*
-
 document
   .getElementById("playerAnalyticsBtn")
-  .addEventListener("click", showPlayerAnalytics); */
+  .addEventListener("click", showPlayerAnalytics);
 
-
-
-  // Event listeners for navigation buttons or links
- document
+// Event listeners for navigation buttons or links
+document
   .getElementById("standingsBtn")
   .addEventListener("click", showStandings);
 
@@ -20,19 +15,18 @@ document
   .getElementById("leaderboardBtn")
   .addEventListener("click", showLeaderboard);
 
-  document
-    .getElementById("teamanalyticsBtn")
-    .addEventListener("click", showTeamAnalytics);
+document
+  .getElementById("teamanalyticsBtn")
+  .addEventListener("click", showTeamAnalytics);
 
-
-
-
-  // Function to show standings section
+// Function to show standings section
 function showStandings() {
-  // Get the standings section
   var standingsSection = document.getElementById("standings-section");
   var leaderboardSection = document.getElementById("playerleaderboard-section");
   var teamanalyticsSection = document.getElementById("teamanalytics-section");
+  var playeranalyticsSection = document.getElementById(
+    "playeranalytics-section"
+  );
 
   // Set the display property to "block" to show the standings section
   standingsSection.style.display = "block";
@@ -42,48 +36,72 @@ function showStandings() {
 
   // Set the display property to "none" to show the leaderboard section
   leaderboardSection.style.display = "none";
+  playeranalyticsSection.style.display = "none";
 }
-
 
 // Function to filter standings by conference
 function filterStandingsByConf() {
-    var selectedConference = document.getElementById("conference-filter").value;
-    var teams = document.getElementsByClassName("team");
-    for (var i = 0; i < teams.length; i++) {
-        var team = teams[i];
-        if (selectedConference === "all" || team.classList.contains(selectedConference)) {
-            team.style.display = "flex";
-        } else {
-            team.style.display = "none";
-        }
+  var selectedConference = document.getElementById("conference-filter").value;
+  var teams = document.getElementsByClassName("team");
+  for (var i = 0; i < teams.length; i++) {
+    var team = teams[i];
+    if (
+      selectedConference === "all" ||
+      team.classList.contains(selectedConference)
+    ) {
+      team.style.display = "flex";
+    } else {
+      team.style.display = "none";
     }
+  }
 }
 
-
-  // Function to show leaderboard senone
+// Function to show leaderboard senone
 function showLeaderboard() {
-  // Get the standings and leaderboard sections
   var standingsSection = document.getElementById("standings-section");
   var leaderboardSection = document.getElementById("playerleaderboard-section");
   var teamanalyticsSection = document.getElementById("teamanalytics-section");
-
+  var playeranalyticsSection = document.getElementById(
+    "playeranalytics-section"
+  );
+  // Set the display property to "block" to show the leaderboard section
+  leaderboardSection.style.display = "block";
   // Set the display property to "none" to show the teamanalytics section
   teamanalyticsSection.style.display = "none";
 
   // Set the display property to "none" to show the standings section
   standingsSection.style.display = "none";
 
-  // Set the display property to "block" to show the leaderboard section
-  leaderboardSection.style.display = "block";
+  playeranalyticsSection.style.display = "none";
 }
 
-
-  // Function to show team analytics section
+// Function to show team analytics section
 function showTeamAnalytics() {
-  // Get the standings and leaderboard sections
   var standingsSection = document.getElementById("standings-section");
   var leaderboardSection = document.getElementById("playerleaderboard-section");
-  var teamanalyticsSection = document.getElementById("teamanalytics-section")
+  var teamanalyticsSection = document.getElementById("teamanalytics-section");
+  var playeranalyticsSection = document.getElementById(
+    "playeranalytics-section"
+  );
+  //Set the display to show team analytics
+  teamanalyticsSection.style.display = "block";
+  // Set the display property to "none" to show the leaderboard section
+  leaderboardSection.style.display = "none";
+
+  // Set the display property to "none" to show the standings section
+  standingsSection.style.display = "none";
+
+  playeranalyticsSection.style.display = "none";
+}
+
+// Function to show team analytics section
+function showPlayerAnalytics() {
+  var standingsSection = document.getElementById("standings-section");
+  var leaderboardSection = document.getElementById("playerleaderboard-section");
+  var teamanalyticsSection = document.getElementById("teamanalytics-section");
+  var playeranalyticsSection = document.getElementById(
+    "playeranalytics-section"
+  );
 
   // Set the display property to "none" to show the leaderboard section
   leaderboardSection.style.display = "none";
@@ -91,6 +109,8 @@ function showTeamAnalytics() {
   // Set the display property to "none" to show the standings section
   standingsSection.style.display = "none";
 
-//Set the display to show team analytics
-  teamanalyticsSection.style.display ="block";
+  //Set the display to show team analytics
+  teamanalyticsSection.style.display = "none";
+
+  playeranalyticsSection.style.display = "block";
 }
